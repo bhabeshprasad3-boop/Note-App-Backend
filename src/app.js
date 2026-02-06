8e3cd6563357f3125ecd202792194676eaf96755
Bhabesh Prasad <bhabeshprasad3@gmail.com>
2026-02-06 14:41:57 +0530

    Fixed CORS for live frontend

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const authRouter = require("./routes/auth.route");
-const noteRouter = require('./routes/note.route');
+const noteRouter = require("./routes/note.route");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
@@ -8,18 +8,20 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: [
+      "http://localhost:5173",
+      "https://note-app-frontend-rose.vercel.app",
+    ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], 
-    allowedHeaders: ["Content-Type", "Authorization"], 
-  })
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
 );
 
 app.use(cookieParser());
-app.use(express.json())
-
+app.use(express.json());
 
 app.use("/api/auth", authRouter);
-app.use("/api",noteRouter)
+app.use("/api", noteRouter);
 
 module.exports = app;
